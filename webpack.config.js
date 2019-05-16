99ea5d057f01b5991f919e018b36d62e9977ba6e
徐昭 <xuzhao.zhique.design>
2019-05-16 20:07:24 +0800

    删除错误配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,6 @@
 module.exports = (env) => {
     if (env && env.NODE_ENV === 'production') {
         return require('./webpack/prod.config.js');
-    } else if (env.NODE_ENV === 'demo') {
-        return require('./webpack/demo.config.js');
     } else {
         return require('./webpack/dev.config.js');
     }
