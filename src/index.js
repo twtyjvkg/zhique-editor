b95da6ad8e53c60ae420e3593c4feac674c0c8b8
xuzhao <xuzhao@zhique.design>
2019-09-29 16:27:45 +0800

    修复bug

--- src/index.js
+++ src/index.js
@@ -1,24 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import { Markdown, MarkdownEditor } from './components';
 
-const ZhiQueEditor = ({ type, ...props }) => {
-    switch (type) {
-        case 'markdown-editor':
-            return (
-                <MarkdownEditor {...props} />
-            );
-        case 'markdown':
-            return (
-                <Markdown {...props} />
-            );
-        default:
-            return (
-                <div>123</div>
-            );
+class ZhiQueEditor extends Component {
+
+    render() {
+        const { type, ...props } = this.props;
+        switch (type) {
+            case 'markdown-editor':
+                return (
+                    <MarkdownEditor {...props} />
+                );
+            case 'markdown':
+                return (
+                    <Markdown {...props} />
+                );
+            default:
+                return (
+                    <div>123</div>
+                );
+        }
     }
-};
+}
 
 ZhiQueEditor.defaultProps = {
   type: 'markdown-editor',
